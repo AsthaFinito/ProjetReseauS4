111caca13d25f2def1e6285d075d157c24b975a2
Valentin Allmnd <121107522+AsthaFinito@users.noreply.github.com>
2023-03-27 11:08:24 +0200

    Ajout de l'encode/decode base64

--- ServeurTCP.c
+++ ServeurTCP.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+	#include <stdio.h>
 #include <stdlib.h> /* pour exit */
 #include <unistd.h> /* pour close */
 #include <sys/types.h>
@@ -22,10 +22,21 @@ int POSITION_TABLEAU = 1;
 char messageEnvoi[LG_MESSAGE];/* le message de la couche Application ! */
 char TableauDecoupage[3][500];
 char TableauDecoupagePosition[2][10];
+char base46_map[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                     'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                     'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+                     
+int b64invs[] = { 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58,
+	59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5,
+	6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+	21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28,
+	29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
+	43, 44, 45, 46, 47, 48, 49, 50, 51 };
 
 
 typedef struct CASE{
-	char couleur[10]; //couleur format RRRGGGBBB
+	char couleur[50]; //couleur format RRRGGGBBB
 
 }CASE;
 
@@ -101,7 +112,18 @@ char* ReturnMatriceDeJeu(CASE Matrice[L][C],char MotAReturn[2000]);
 void SetPixelCommande(CASE Matrice[L][C], int ChoixLigne, int ChoixColonne, char NewCouleur[10]);
 int DecoupeMessageSetPixel(char* MessageComplet);
 void TestDecoupageMessage();
-void TestPixelOutOfBound(char MessagePixel[20]);
+void TestPixelOutOfBound(char MessagePixel[20],char MessagePixel2[20]);
+void TestBadColor(char MessageColor[20],int Ligne,int Colonne);
+
+char base64_to_ascii(char base64);
+char* base64_to_bits(char* cipher);
+size_t b64_decoded_size(const char *in);
+char* base64_encode(char* plain);
+int b64_decode(const char *in, unsigned char *out, size_t outlen);
+int b64_isvalidchar(char c);
+size_t b64_encoded_size(size_t inlen);
+int TestChiffreDansBits(char* IN);
+
 
 
 
@@ -327,7 +349,7 @@ int DecoupeMessageSetPixel(char* MessageComplet){
 			}
 			
 			
-			strcpy(messageEnvoi,"00 OK");
+			//strcpy(messageEnvoi,"00 OK");
 			
 		}
 		
@@ -340,8 +362,241 @@ int DecoupeMessageSetPixel(char* MessageComplet){
 	return 1;
 	
 }
+int TestChiffreDansBits(char* IN){
 
-void TestPixelOutOfBound(char MessagePixel[20]){
+	for(int i=0;i<9;i++){
+	
+		printf("Valeur test en int : %d \n",(int)IN[i]);
+		if((int)IN[i]>=48 && (int)IN[i]<=57){
+		
+			printf("ok %d \n",i);
+			printf("Valeur test : %c \n",IN[i]);
+			
+			
+		}
+		else{
+			printf("Erreur dans le test , pas un chiffre \n");
+			strcpy(messageEnvoi,"12 Bad Color");
+			return -1;
+		}
+	}
+	return 0;
+}
+
+void TestBadColor(char MessageColor[20],int Ligne,int Colonne){
+
+	printf("La couleur est : %s \n",MessageColor);
+	
+	char CHAINE_ENCODE[40];
+	char CHAINE_DECODE[40];
+	char test;
+	char test2;
+	MessageColor[strlen(MessageColor) - 1] = '\0';
+	printf("Valeur du strlen :%d \n",strlen(MessageColor));
+	
+	
+	
+		printf("Test en cours \n");
+		
+		
+			
+			strcpy(CHAINE_ENCODE,base64_encode(MessageColor)); // encode en base64 
+			unsigned int SIZE_OF_ENCODE=b64_encoded_size(strlen(CHAINE_ENCODE));
+			printf(" [%s] encoded \n",CHAINE_ENCODE); //j'affiche
+			printf("Valeur de b64_encoded_size : %d \n",SIZE_OF_ENCODE);
+			//test=base64_to_ascii(test2);
+			//strcpy(NEWSTR,base64_to_bits(NEWSTR2)); //conversion bits 
+			int test_valeur=b64_decode(CHAINE_ENCODE,CHAINE_DECODE,10);
+			
+			//printf("Chaine décode ? [%c] \n",test);
+			//printf("Chaine décode2 ? [%s] \n",NEWSTR);
+			printf("Chaine décode2 ? [%02x] \n",CHAINE_DECODE);
+			printf("Chaine décode2 en s ? [%s] \n",CHAINE_DECODE);
+			//unsigned int test_size=b64_decoded_size(NEWSTR);
+			unsigned int test_size=b64_decoded_size(CHAINE_DECODE);
+			printf("Valeur de b64_decoded_size : %d \n",test_size);
+			int test_chiffre=TestChiffreDansBits(CHAINE_DECODE);
+			if(test_chiffre==-1){
+			
+				strcpy(messageEnvoi,"12 Bad Color");
+			}
+			else{
+				printf("Avant modif de matrice \n");
+				printf("Valeur de ligne et de colonne : [%d] [%d] \n",Ligne,Colonne);
+				strcpy(MatriceDeJeu[Ligne][Colonne].couleur,CHAINE_ENCODE);
+				printf("Après modif de matrice \n");
+				strcpy(messageEnvoi,"00 OK");
+			
+			}
+			
+		
+		
+		
+	
+	
+
+
+}
+size_t b64_encoded_size(size_t inlen)
+{
+	size_t ret;
+
+	ret = inlen;
+	if (inlen % 3 != 0)
+		ret += 3 - (inlen % 3);
+	ret /= 3;
+	ret *= 4;
+
+	return ret;
+}
+int b64_decode(const char *in, unsigned char *out, size_t outlen)
+{
+	size_t len;
+	size_t i;
+	size_t j;
+	int    v;
+
+	if (in == NULL || out == NULL)
+		return 0;
+
+	len = strlen(in);
+	if (outlen < b64_decoded_size(in) || len % 4 != 0)
+		return 0;
+
+	for (i=0; i<len; i++) {
+		if (!b64_isvalidchar(in[i])) {
+			return 0;
+		}
+	}
+
+	for (i=0, j=0; i<len; i+=4, j+=3) {
+		v = b64invs[in[i]-43];
+		v = (v << 6) | b64invs[in[i+1]-43];
+		v = in[i+2]=='=' ? v << 6 : (v << 6) | b64invs[in[i+2]-43];
+		v = in[i+3]=='=' ? v << 6 : (v << 6) | b64invs[in[i+3]-43];
+
+		out[j] = (v >> 16) & 0xFF;
+		if (in[i+2] != '=')
+			out[j+1] = (v >> 8) & 0xFF;
+		if (in[i+3] != '=')
+			out[j+2] = v & 0xFF;
+	}
+
+	return 1;
+}
+int b64_isvalidchar(char c)
+{
+	if (c >= '0' && c <= '9')
+		return 1;
+	if (c >= 'A' && c <= 'Z')
+		return 1;
+	if (c >= 'a' && c <= 'z')
+		return 1;
+	if (c == '+' || c == '/' || c == '=')
+		return 1;
+	return 0;
+}
+
+char* base64_encode(char* plain) {
+
+    char counts = 0;
+    char buffer[3];
+    char* cipher = malloc(strlen(plain) * 4 / 3 + 4);
+    int i = 0, c = 0;
+
+    for(i = 0; plain[i] != '\0'; i++) {
+        buffer[counts++] = plain[i];
+        if(counts == 3) {
+            cipher[c++] = base46_map[buffer[0] >> 2];
+            cipher[c++] = base46_map[((buffer[0] & 0x03) << 4) + (buffer[1] >> 4)];
+            cipher[c++] = base46_map[((buffer[1] & 0x0f) << 2) + (buffer[2] >> 6)];
+            cipher[c++] = base46_map[buffer[2] & 0x3f];
+            counts = 0;
+        }
+    }
+
+    if(counts > 0) {
+        cipher[c++] = base46_map[buffer[0] >> 2];
+        if(counts == 1) {
+            cipher[c++] = base46_map[(buffer[0] & 0x03) << 4];
+            cipher[c++] = '=';
+        } else {                      // if counts == 2
+            cipher[c++] = base46_map[((buffer[0] & 0x03) << 4) + (buffer[1] >> 4)];
+            cipher[c++] = base46_map[(buffer[1] & 0x0f) << 2];
+        }
+        cipher[c++] = '=';
+    }
+
+    cipher[c] = '\0';   /* string padding character */
+    return cipher;
+}
+
+size_t b64_decoded_size(const char *in)
+{
+	size_t len;
+	size_t ret;
+	size_t i;
+
+	if (in == NULL)
+		return 0;
+
+	len = strlen(in);
+	ret = len / 4 * 3;
+
+	for (i=len; i-->0; ) {
+		if (in[i] == '=') {
+			ret--;
+		} else {
+			break;
+		}
+	}
+
+	return ret;
+}
+
+char* base64_to_bits(char* cipher) {
+
+    char counts = 0;
+    char buffer[4];
+    char* plain = malloc(strlen(cipher) * 3 / 4);
+    int i = 0, p = 0;
+
+    for(i = 0; cipher[i] != '\0'; i++) {
+        char k;
+        for(k = 0 ; k < 64 && base46_map[k] != cipher[i]; k++);
+        buffer[counts++] = k;
+        if(counts == 4) {
+            plain[p++] = (buffer[0] << 2) + (buffer[1] >> 4);
+            if(buffer[2] != 64)
+                plain[p++] = (buffer[1] << 4) + (buffer[2] >> 2);
+            if(buffer[3] != 64)
+                plain[p++] = (buffer[2] << 6) + buffer[3];
+            counts = 0;
+        }
+    }
+
+    plain[p] = '\0';    /* string padding character */
+    return plain;
+}
+char base64_to_ascii(char base64) {
+	printf("Dans décode 1 carac \n");
+    char* table_base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    char* position = strchr(table_base64, base64);
+    int index = (int)(position - table_base64);
+	
+    if (position == NULL) {
+        return '\0';
+    }
+	printf("Après le if \n");
+    unsigned char ascii = index;
+	printf("Après le ascii \n");
+    return (char)ascii;
+}
+
+
+
+
+void TestPixelOutOfBound(char MessagePixel[20],char MessagePixel2[20]){
 
 	char *token;
 	char Separateur[2]="x";
@@ -363,14 +618,17 @@ void TestPixelOutOfBound(char MessagePixel[20]){
 	
 		printf("Erreur dans les coo (ligne) \n");
 		strcpy(messageEnvoi,"12 PixelOutOfBond \n");
+		return;
 		
 	}
 	else if(atoi(TableauDecoupagePosition[1])<0 || atoi(TableauDecoupagePosition[1])>C){
 		printf("Erreur dans les coo (colonne) \n");
 		strcpy(messageEnvoi,"12 PixelOutOfBond \n");
+		return;
 	}
 	else{
 		printf("Coordonnées Valide \n");
+		TestBadColor(MessagePixel2,atoi(TableauDecoupagePosition[0]),atoi(TableauDecoupagePosition[1]));
 	}
 
 }
@@ -383,7 +641,8 @@ void TestDecoupageMessage(){
 	strcpy(test2,TableauDecoupage[2]);
 	printf("Chaine 1 :%s \n",test1);
 	printf("Chaine 2 :%s \n",test2);
-	TestPixelOutOfBound(test1);
+	TestPixelOutOfBound(test1,test2);
+	//TestBadColor(test2);
 	
 	
 	
@@ -438,6 +697,7 @@ void MessageADecomposer(char Message[LG_MESSAGE]){
 	else{
 		printf("Bad command \n");
 		int test=DecoupeMessageSetPixel(Message);
+		
 		if(test==-1){
 		
 			printf("Mauvaise Commande \n");
