3972adab39ef62642051e2b0e6f3c1ba0ff48fd1
Valentin Allmnd <121107522+AsthaFinito@users.noreply.github.com>
2023-03-27 22:01:16 +0200

    Client avec menu

--- ClientTCP.c
+++ ClientTCP.c
@@ -10,9 +10,19 @@
 #define LG_MESSAGE 256
 #define PORT IPPORT_USERRESERVED //5000
 
+char messageRecu[LG_MESSAGE];/* le message de la couche Application ! */
+	char messageEnvoi[LG_MESSAGE];/* le message de la couche Application ! */
+char base46_map[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                     'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                     'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
 
 void TestLancementClient(int argc, char *argv[]);
 void InterpretationMessageBoucle(char *MessageRecu);
+char* base64_encode(char* plain);
+int Menu();
+int InterpretationMenu(char input);
+
 
 
 
@@ -61,18 +71,22 @@ int main(int argc, char *argv[]){
 	}
 	printf("Connexion au serveur ?\n");
 	int ecrits, lus;/* nb d’octets ecrits et lus */
-	char messageRecu[LG_MESSAGE];/* le message de la couche Application ! */
-	char messageEnvoi[LG_MESSAGE];/* le message de la couche Application ! */
+	
 	char InputClient[LG_MESSAGE];
 	memset(messageEnvoi, 0x00, LG_MESSAGE*sizeof(char));
 	memset(messageRecu, 0x00, LG_MESSAGE*sizeof(char));
-	
+	printf("Bienvenue dans le pixel war du groupe A05 \n");
+	int test_SortieMenu=0;
 	while(1){
 	
-		fgets(InputClient, sizeof(InputClient), stdin);
-		strcpy(messageEnvoi, InputClient);
+		//fgets(InputClient, sizeof(InputClient), stdin);
+		//strcpy(messageEnvoi, InputClient);
+		while(test_SortieMenu==0){
+			test_SortieMenu=Menu();
+		}
+		test_SortieMenu=0;
 		ecrits = write(socketEcoute, messageEnvoi, strlen(messageEnvoi));// message à TAILLE variable
-		//printf("Avant le switch ecrit \n");
+		printf("Avant le switch ecrit \n");
 		switch(ecrits)
 		{
 			case-1 :/* une erreur ! */
@@ -81,11 +95,12 @@ int main(int argc, char *argv[]){
 				exit(-3);
 				break;
 			case 0 :/* la socket est fermée */
-				fprintf(stderr, "La socket a été fermée par le serveur !\n\n");
-				close(socketEcoute);
-				exit(-1);
+				//fprintf(stderr, "La socket a été fermée par le serveur !\n\n");
+				//close(socketEcoute);
+				//exit(-1);
 				break;
 			default:
+				//printf("Dans le default \n");
 				break;
 
 		}
@@ -99,9 +114,10 @@ int main(int argc, char *argv[]){
 				close(socketEcoute);
 				exit(-4);
 			case 0 :/* la socket est fermée */
-				fprintf(stderr, "La socket a été fermée par le serveur !\n\n");
-				close(socketEcoute);
-				exit(-1);
+				//fprintf(stderr, "La socket a été fermée par le serveur !\n\n");
+				//close(socketEcoute);
+				//exit(-1);
+				break;
 			default:/* réception de n octets */
 				printf("Dans le default \n");
 				messageRecu[lus]='\0';
@@ -142,9 +158,108 @@ void TestLancementClient(int argc, char *argv[]){
 
 
 }
+int InterpretationMenu(char input){
+
+	if(input=='a'){
+		printf("Vous avez choisi le getSize \n");
+		strcpy(messageEnvoi,"/getSize\n");
+		return 1;
+	}
+	else if(input=='b'){
+	
+		printf("Vous avez choisi le getLimits \n");
+		strcpy(messageEnvoi,"/getLimits\n");
+		return 1;
+	}
+	else if(input=='c'){
+	
+		printf("Vous avez choisi le getMatrice \n");
+		strcpy(messageEnvoi,"/getMatrice\n");
+		return 1;
+	}
+	else if(input=='d'){
+	
+		printf("Vous avez choisi le setPixel \n");
+		strcpy(messageEnvoi,"/setPixel 3x3 128128128\n");
+		return 1;
+	}
+	else if(input=='e'){
+	
+		printf("Vous avez choisi le getWaitTime \n");
+		//strcpy(messageEnvoi,"/getSize");
+		return 1;
+	}
+	else{
+		printf("Erreur dans le choix d'action \n");
+		return 0;
+	}
+
+
+}
+
+int Menu(){
+
+	
+	printf("Choissisez une fonction : \n");
+	printf("	A - getSize \n");
+	printf("	B - getLimits \n");
+	printf("	C - getMatrice \n");
+	printf("	D - setPixel \n");
+	printf("	E - getWaitTime \n");
+	char input;
+	scanf("%c",&input);
+	
+	printf("Voici la touche tapé : [%c] \n",input);
+	int test_FinInterpretation=InterpretationMenu(input);
+	if(test_FinInterpretation==1){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+	
+	
+
+}
 
 void InterpretationMessageBoucle(char *MessageRecu){
 
-	printf("Interprétation en cours \n");
-	printf("Le message recu est : %s \n",MessageRecu);
+	//printf("Interprétation en cours \n");
+	printf("Reponse du serveur : %s ",MessageRecu);
 }
+
+
+char* base64_encode(char* plain) {
+
+    char counts = 0;
+    char buffer[3];
+    char* cipher = malloc(strlen(plain) * 4 / 3 + 4);
+    int i = 0, c = 0;
+
+    for(i = 0; plain[i] != '\0'; i++) {
+        buffer[counts++] = plain[i];
+        if(counts == 3) {
+            cipher[c++] = base46_map[buffer[0] >> 2];
+            cipher[c++] = base46_map[((buffer[0] & 0x03) << 4) + (buffer[1] >> 4)];
+            cipher[c++] = base46_map[((buffer[1] & 0x0f) << 2) + (buffer[2] >> 6)];
+            cipher[c++] = base46_map[buffer[2] & 0x3f];
+            counts = 0;
+        }
+    }
+
+    if(counts > 0) {
+        cipher[c++] = base46_map[buffer[0] >> 2];
+        if(counts == 1) {
+            cipher[c++] = base46_map[(buffer[0] & 0x03) << 4];
+            cipher[c++] = '=';
+        } else {                      // if counts == 2
+            cipher[c++] = base46_map[((buffer[0] & 0x03) << 4) + (buffer[1] >> 4)];
+            cipher[c++] = base46_map[(buffer[1] & 0x0f) << 2];
+        }
+        cipher[c++] = '=';
+    }
+
+    cipher[c] = '\0';   /* string padding character */
+    return cipher;
+}
+
